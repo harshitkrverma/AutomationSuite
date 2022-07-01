8ae5bbfe67d9c26e7a7b4fb85353eb531f56e0fa
harshit-cloudsufi <harshit.verma@cloudsufi.com>
2022-07-02 03:51:24 +0530

    Init

---
+++ src/main/java/mail/harshitkumarvermaAtgmail/com/App.java
@@ -0,0 +1,13 @@
+package mail.harshitkumarvermaAtgmail.com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/mail/harshitkumarvermaAtgmail/com/AppTest.java
@@ -0,0 +1,20 @@
+package mail.harshitkumarvermaAtgmail.com;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
