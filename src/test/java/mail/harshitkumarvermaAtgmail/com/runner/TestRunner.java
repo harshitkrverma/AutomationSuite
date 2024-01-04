8cb40ac9c5f10f2136e9d77d756f7384fbda0bf5
harshitkrverma <harshitkumarverma@gmail.com>
2024-01-04 23:53:37 +0530

    Selenium maven

--- src/test/java/mail/harshitkumarvermaAtgmail/com/runner/TestRunner.java
+++ src/test/java/mail/harshitkumarvermaAtgmail/com/runner/TestRunner.java
@@ -8,7 +8,6 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = {"src/test/features/"},
         glue = {"mail.harshitkumarvermaAtgmail.com.stepdef", "mail.harshitkumarvermaAtgmail.com.utils"},
-        tags = "@Test_id_TC01",
         plugin = {
                 "pretty",
                 "html:target/cucumber-reports/cucumber.html",
