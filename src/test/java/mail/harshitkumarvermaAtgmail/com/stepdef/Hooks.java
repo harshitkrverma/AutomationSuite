265e19ad595dac3f52a3948abe3bd4c6eeb085e5
harshitkrverma <harshitkumarverma@gmail.com>
2024-02-11 15:10:33 +0530

    SeleniumHelper Chrome options

--- src/test/java/mail/harshitkumarvermaAtgmail/com/stepdef/Hooks.java
+++ src/test/java/mail/harshitkumarvermaAtgmail/com/stepdef/Hooks.java
@@ -1,24 +1,14 @@
 package mail.harshitkumarvermaAtgmail.com.stepdef;
 
 import io.cucumber.java.AfterStep;
-import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
 import mail.harshitkumarvermaAtgmail.com.utils.SeleniumHelper;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 
-import java.util.Random;
-
 
 public class Hooks {
-    @Before
-    public static void beforeScenario(Scenario scenario){
-        System.out.println("getId "+scenario.getId());
-        System.out.println("getLine "+scenario.getLine());
-        System.out.println("getName "+scenario.getName());
-
-    }
 
     @AfterStep
     public static void afterStep(Scenario scenario){
@@ -27,6 +17,7 @@ public class Hooks {
             byte[] screenshotBytes = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
             scenario.attach(screenshotBytes, "image/png", String.valueOf(scenario.getLine()));
         } catch (Exception e) {
+            System.out.println(e);
         }
     }
 
--- src/test/java/mail/harshitkumarvermaAtgmail/com/utils/SeleniumHelper.java
+++ src/test/java/mail/harshitkumarvermaAtgmail/com/utils/SeleniumHelper.java
@@ -1,7 +1,6 @@
 package mail.harshitkumarvermaAtgmail.com.utils;
 
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
